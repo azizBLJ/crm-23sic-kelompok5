f5f03fdc15454ed0fdee4688f27d901c573f2aa6
Muhammad Abdul Aziz <abdul23si@mahasiswa.pcr.ac.id>
2025-05-23 08:35:34 +0700

    Starter projek by kelompok5

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import MainLayout from './components/MainLayout'
+import Dashboard from './pages/Dashboard'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Routes>
+      <Route  element={<MainLayout />}>
+        <Route path="/" element={<Dashboard />}/>
+      </Route>
+    </Routes>
+    </>
+  )
+}
+
+export default App
