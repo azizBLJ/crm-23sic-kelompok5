7e87cb49a74c4f2ca26251f28975b80eb12fae50
Amelia Dhea Putri <dhea23si@mahasiswa.pcr.ac.id>
2025-05-23 09:29:24 +0700

    selesai produk by dhea

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './components/MainLayout'
 import Dashboard from './pages/Dashboard'
+import SalesManagement from './pages/SalesManagement'
+import ProductManagement from './pages/Produk'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -13,6 +15,8 @@ function App() {
     <Routes>
       <Route  element={<MainLayout />}>
         <Route path="/" element={<Dashboard />}/>
+        <Route path="/penjualan" element={<SalesManagement />}/>
+        <Route path="/produk" element={<ProductManagement />}/>
       </Route>
     </Routes>
     </>
